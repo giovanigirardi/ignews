04aa123eff0e2ef20a3af7fe6a92060f24d01d3e
Giovani Girardi <giovanigg08@gmail.com>
2022-01-23 16:37:10 -0300

    feat: Criada estrutura principal do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default Home;
