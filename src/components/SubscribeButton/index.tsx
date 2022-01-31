ddd9b189a7ba1013f4d0745a4f4e35c77c85a33d
Giovani Girardi <giovanigg08@gmail.com>
2022-01-30 21:57:26 -0300

    feat: criada checkoutSession do Stripe; Criado customer no stripe utilizando user do FaunaDB

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from "next-auth/react";
+
+import { api } from "../../services/api";
+import { getStripeJS } from "../../services/stripe-js";
+
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -7,8 +12,34 @@ interface SubscribeButtonProps {
 export const SubscribeButton: React.FC<SubscribeButtonProps> = ({
   priceId,
 }) => {
+  const { data } = useSession();
+
+  const handleSubscribe = async () => {
+    if (!data) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("subscribe", { priceId });
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+      console.error(error);
+    }
+  };
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
