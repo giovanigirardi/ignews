87e36829aa601c280a9347ad76bb29246de15ead
Giovani Girardi <giovanigg08@gmail.com>
2022-03-24 20:06:19 -0300

    feat: Finalizado componente de Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,26 @@
+import { useRouter } from "next/router";
+
 import { SignInButton } from "../SignInButton";
+import { ActiveLink } from "../ActiveLink";
 
 import styles from "./styles.module.scss";
 
 export const Header = () => {
+  const { asPath } = useRouter();
+
+  console.log(asPath);
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news logo" />
         <nav>
-          <a className={styles.active} href="">
-            Home
-          </a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
